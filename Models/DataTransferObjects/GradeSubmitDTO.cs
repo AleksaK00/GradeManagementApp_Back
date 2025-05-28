960970a1b8ac82704782cf03f5701fc24187741b
AleksaK00 <aleksa.karamarkovic@gmail.com>
2025-05-28 10:35:11 +0200

    Verzija 1.1.1 Excel update - Validation

--- Models/DataTransferObjects/GradeSubmitDTO.cs
+++ Models/DataTransferObjects/GradeSubmitDTO.cs
@@ -1,10 +1,15 @@
-﻿namespace GradeManagementApp_Back.Models.DataTransferObjects
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GradeManagementApp_Back.Models.DataTransferObjects
 {
     public class GradeSubmitDTO
     {
         public int? Id { get; set; }
+        [Required]
         public int SkolskaGodina { get; set; }
+        [Required]
         public int Razred { get; set; }
+        [Required]
         public int Program { get; set; }
     }
 }
